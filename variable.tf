b42d363216063272280dd0ad91b0bff824b1ef54
harish lad <harish.lad@gmail.com>
2019-01-23 16:59:25 +0000

    new files

---
+++ variable.tf
@@ -0,0 +1,20 @@
+variable "access_key" 
+{
+	default = "AKIAJ4EZ6G6TM4NIYLPQ"
+}
+
+variable "secret_key"
+{
+	default = "NU42O/t9VJ5sfniNCFFnJlCM08+9PD+MS41ur1JQ"
+}
+
+variable "instance_type"
+{
+	default = "t2.micro"
+}
+
+variable "region"
+{
+	default = "ap-south-1"
+}
+
